c09d336a638c7695dcd70ef8420545029f4a2566
Naveen <morishettynaveen9@gmail.com>
2025-05-17 13:20:42 +0530

    chore: githubcode

--- src/product-catalog/main.go
+++ src/product-catalog/main.go
@@ -252,7 +252,7 @@ func (p *productCatalog) GetProduct(ctx context.Context, req *pb.GetProductReque
 
 	// GetProduct will fail on a specific product when feature flag is enabled
 	if p.checkProductFailure(ctx, req.Id) {
-		msg := fmt.Sprintf("Error: Product Catalog Fail Feature Flag Enabled")
+		msg := fmt.("Error: Product Catalog Fail Feature Flag Enabled")
 		span.SetStatus(otelcodes.Error, msg)
 		span.AddEvent(msg)
 		return nil, status.Errorf(codes.Internal, msg)
@@ -309,12 +309,12 @@ func (p *productCatalog) checkProductFailure(ctx context.Context, id string) boo
 	return failureEnabled
 }
 
-func createClient(ctx context.Context, svcAddr string) (*grpc.ClientConn, error) {
-	return grpc.DialContext(ctx, svcAddr,
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
-		grpc.WithStatsHandler(otelgrpc.NewClientHandler()),
-	)
-}
+// func createClient(ctx context.Context, svcAddr string) (*grpc.ClientConn, error) {
+// 	return grpc.DialContext(ctx, svcAddr,
+// 		grpc.WithTransportCredentials(insecure.NewCredentials()),
+// 		grpc.WithStatsHandler(otelgrpc.NewClientHandler()),
+// 	)
+// }
 
 
 
